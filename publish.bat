cab25cdf18ec84e1816036888b0551957a129703
445939112@qq.com <Xjq**gb5059>
2025-11-06 11:54:47 +0800

    [M]publish.bat 独立部署压缩后45M

--- publish.bat
+++ publish.bat
@@ -1,7 +1,9 @@
 SET CUR_PATH=%CD%
 SET OUTPUT=%CUR_PATH%\publish
-REM dotnet publish -c Release -o ./publish
-dotnet publish -c Release -r win-x64 -o ./publish  /p:PublishSingleFile=true /p:DebugType=None /p:DebugSymbols=false --framework netcoreapp3.1
+REM publish -c Release -o ./publish
+REM dotnet publish -c Release -o ./publish /p:PublishSingleFile=true /p:SelfContained=true
+REM 独立部署
+dotnet publish -c Release -r win-x64 -o ./publish  /p:PublishSingleFile=true /p:SelfContained=true /p:DebugType=None /p:DebugSymbols=false --framework netcoreapp3.1
 cd %OUTPUT%
-del *.pdb
-del Launcher.deps.json
+REM del *.pdb
+REM del Launcher.deps.json
