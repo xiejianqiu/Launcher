a276f9afdd1199ad09e2b03cd1aa3ebecb957e1c
445939112@qq.com <Xjq**gb5059>
2025-11-06 11:33:01 +0800

    [M]独立包体

--- publish.bat
+++ publish.bat
@@ -1,6 +1,7 @@
 SET CUR_PATH=%CD%
 SET OUTPUT=%CUR_PATH%\publish
-dotnet publish -c Release -o ./publish
+REM dotnet publish -c Release -o ./publish
+dotnet publish -c Release -r win-x64 -o ./publish  /p:PublishSingleFile=true /p:DebugType=None /p:DebugSymbols=false --framework netcoreapp3.1
 cd %OUTPUT%
 del *.pdb
 del Launcher.deps.json
