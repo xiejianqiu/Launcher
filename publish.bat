ec097964d91a445dcd9f2dad3749ab3a044b3123
445939112@qq.com <Xjq**gb5059>
2025-11-11 19:36:24 +0800

    [M]publish.bat

--- publish.bat
+++ publish.bat
@@ -5,6 +5,7 @@ REM dotnet publish -c Release -o ./publish /p:PublishSingleFile=true /p:SelfCont
 SET PARGMA=-p:AssemblyName="범인수선" -p:Authors="범인수선"
 REM 独立部署
 dotnet publish -c Release -r win-x64 -o ./publish -p:AssemblyName="Game Launch" /p:PublishSingleFile=true /p:SelfContained=true /p:DebugType=None /p:DebugSymbols=false --framework netcoreapp3.1
+REM dotnet publish -c Release -r win-x64 -o ./publish -p:AssemblyName="범인수선" -p:Authors="범인수선" /p:PublishSingleFile=true /p:SelfContained=true /p:DebugType=None /p:DebugSymbols=false --framework netcoreapp3.1
 cd %OUTPUT%
 REM del *.pdb
 REM del Launcher.deps.json
