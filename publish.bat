082cc0f5b065cbc933f8ec781c0f5d2f2cb92be0
445939112@qq.com <Xjq**gb5059>
2025-11-11 19:43:57 +0800

    [M]publish.bat

--- publish.bat
+++ publish.bat
@@ -2,6 +2,7 @@ SET CUR_PATH=%CD%
 SET OUTPUT=%CUR_PATH%\publish
 REM publish -c Release -o ./publish
 REM dotnet publish -c Release -o ./publish /p:PublishSingleFile=true /p:SelfContained=true
+SET PARGMA=-p:AssemblyName="Game Launch" -p:Authors="Game Launch"
 dotnet publish -c Release -r win-x64 -o ./publish -p:AssemblyName="Game Launch" /p:PublishSingleFile=true /p:SelfContained=true /p:DebugType=None /p:DebugSymbols=false --framework netcoreapp3.1
 REM dotnet publish -c Release -r win-x64 -o ./publish -p:AssemblyName="범인수선" -p:Authors="범인수선" /p:PublishSingleFile=true /p:SelfContained=true /p:DebugType=None /p:DebugSymbols=false --framework netcoreapp3.1
 cd %OUTPUT%
