f693077e616bf12361f68c4351d260069f252613
445939112@qq.com <Xjq**gb5059>
2025-11-11 17:41:42 +0800

    [M]publish.bat

--- publish.bat
+++ publish.bat
@@ -3,7 +3,7 @@ SET OUTPUT=%CUR_PATH%\publish
 REM publish -c Release -o ./publish
 REM dotnet publish -c Release -o ./publish /p:PublishSingleFile=true /p:SelfContained=true
 REM 独立部署
-dotnet publish -c Release -r win-x64 -o ./publish -p:AssemblyName="Game Launch"  /p:PublishSingleFile=true /p:SelfContained=true /p:DebugType=None /p:DebugSymbols=false --framework netcoreapp3.1
+dotnet publish -c Release -r win-x64 -o ./publish -p:AssemblyName="Game Launch" -p:Authors="범인수선"  /p:PublishSingleFile=true /p:SelfContained=true /p:DebugType=None /p:DebugSymbols=false --framework netcoreapp3.1
 cd %OUTPUT%
 REM del *.pdb
 REM del Launcher.deps.json
