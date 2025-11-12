6f085be82d49aeca46721d73980c95274e907914
445939112@qq.com <Xjq**gb5059>
2025-11-12 14:59:33 +0800

    [M]publish.bat

--- publish.bat
+++ publish.bat
@@ -1,11 +1,8 @@
-REM Í¸?Ù¤ÖµÓðÍóð¤÷»?ú¼£¬Üú??Ùþ?????
-SET CUR_PATH=%CD%
+SET CUR_PATH=%~dp0
 SET OUTPUT=%CUR_PATH%\publish
+RD /S /Q %OUTPUT%
 REM publish -c Release -o ./publish
 REM dotnet publish -c Release -o ./publish /p:PublishSingleFile=true /p:SelfContained=true
 SET PARGMA=-p:AssemblyName="Game Launch" -p:Authors="Game Launch"
 dotnet publish -c Release -r win-x64 -o ./publish -p:AssemblyName="Game Launch" /p:PublishSingleFile=true /p:SelfContained=true /p:DebugType=None /p:DebugSymbols=false --framework netcoreapp3.1
 REM dotnet publish -c Release -r win-x64 -o ./publish -p:AssemblyName="¹üÀÎ¼ö¼±" -p:Authors="¹üÀÎ¼ö¼±" /p:PublishSingleFile=true /p:SelfContained=true /p:DebugType=None /p:DebugSymbols=false --framework netcoreapp3.1
-cd %OUTPUT%
-REM del *.pdb
-REM del Launcher.deps.json
